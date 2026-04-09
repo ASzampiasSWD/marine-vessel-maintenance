e29e71f94a19fe0f4e2996610fada5b2794d2c80
Amanda Szampias <szampiam@amazon.com>
2026-04-09 00:26:24 -0400

    db.js

---
+++ db.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  user: process.env.DB_USER,
+  host: process.env.HOST,
+  database: process.env.DATABASE,
+  password: process.env.PASSWORD,
+  port: parseInt(process.env.PORT)
+});
+
+// Export a query function to easily interact with the pool
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
